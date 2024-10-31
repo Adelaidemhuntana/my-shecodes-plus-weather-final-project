c03c54272d87e8b345ab30fdf4a4d5f4b5e99fd2
Adelaidemhuntana <163135758+Adelaidemhuntana@users.noreply.github.com>
2024-10-31 21:25:36 +0200

    Added the search functionality using the api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input"); // assuming it’s a class
- let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+function refreshWeather(response) {
+  // Correct the selector to use the ID
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  // Update the temperature in the HTML
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function searchCity(city) {
+  let apiKey = "d7bft2a2ecbd3c41d91cf26o4a04c0b3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
+  // Make the API call with axios and call refreshWeather when it completes
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  
+  searchCity(searchInput.value);
+}
+
+// Add the event listener to the form
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("pretoria")
